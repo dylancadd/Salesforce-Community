3c52c127667079f404aa2cd5b1dcda1e0de1d2f0
Dylan Cadd <djcadd281@gmail.com>
2019-12-04 00:29:53 -0600

    Refactored phoneNumber1

--- Advanced_Components/force-app/main/default/lwc/phoneNumber1/phoneNumber1.js
+++ Advanced_Components/force-app/main/default/lwc/phoneNumber1/phoneNumber1.js
@@ -1,8 +1,21 @@
 import { LightningElement, track } from 'lwc';
 
 export default class PhoneNumber1 extends LightningElement {
-    @track val = 0;
-    changeHandler(event) {
-        this.val = event.target.value;
+    @track areaCode = 608;
+    @track first = 921;
+    @track last = 3103;
+
+
+    changeAreaCode(event) {
+        this.areaCode = event.target.value;
     }
+    changeFirst(event) {
+        this.first = event.target.value;
+    }
+    changeLast(event) {
+        this.last = event.target.value;
+    }
+
+    
+
 }
