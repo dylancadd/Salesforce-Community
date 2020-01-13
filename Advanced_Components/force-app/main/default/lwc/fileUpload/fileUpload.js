b99aef38afd07289ff1ae29b1a25a2b8b9236387
Dylan Cadd <djcadd281@gmail.com>
2020-01-13 13:37:18 -0600

    Updated fileUpload

---
+++ Advanced_Components/force-app/main/default/lwc/fileUpload/fileUpload.js
@@ -0,0 +1,18 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class FileUpload extends LightningElement {
+
+    @api myRecordId;
+    @track uploadedFile;
+
+    get acceptedFormats() {
+        return ['.pdf', '.png', '.jpg'];
+    }
+
+    handleUploadFinished(event) {
+
+        const uploadedFiles = event.detail.files;
+        this.uploadedFile = uploadedFiles[0].name;
+
+    }
+}
